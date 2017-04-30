34e5de92d00f6f83a72916175d3bebe444bd0bd1
abhrp <abhiroop.patel@gmail.com>
2017-04-30 13:00:27 +0530

    Added install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Building..."
+./gradlew assembleDebug --configure-on-demand --daemon
+for SERIAL in $(adb devices | grep -v List | cut -f 1);
+    do
+        DEVICE=$(adb -s $SERIAL shell getprop ro.product.model)
+        echo "Installing On $DEVICE"
+        adb -s $SERIAL install -r app/build/outputs/apk/app-debug.apk
+        echo "Launching On $DEVICE"
+        adb -s $SERIAL shell am start -n "com.github.abhrp.pixabaysearchdemo/com.github.abhrp.pixabaysearchdemo.PixabaySearchActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+    done
